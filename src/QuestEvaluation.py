ca816b37a225fda48809c5d3762880f3cf741665
Ash0ne <929869708@qq.com>
2019-01-19 21:15:05 +0800

    小调整

--- src/QuestEvaluation.py
+++ src/QuestEvaluation.py
@@ -1,5 +1,4 @@
 from fractions import Fraction
-from math import gcd
 
 class Evaluation:
 
@@ -14,6 +13,8 @@ class Evaluation:
         elif operator == "*":
             return num1 * num2
         elif operator == "/":
+            if num2 == 0:
+                raise Exception("Divisor = 0")
             return Fraction(num1, num2)
         elif operator == "^":
             return pow(num1, num2)
@@ -23,47 +24,37 @@ class Evaluation:
         num2 = data.pop()
         num1 = data.pop()
         num3 = self.getvalue(num1, num2, operator)
-        if num3:
-            data.append(num3)
+        data.append(num3)
 
-    def Caculator(self):
+    def Caculator(self, line):
         # 中缀表达式转换为后缀表达式的过程中同时计算结果
-        r = open("QuestGen\\src\\solve-me.txt", 'r', encoding = 'UTF-8')
-        lines = r.readlines()
-        for line in lines:
-            opt = []
-            data = []
-            i = 0
-            while i < len(line):
-                if line[i] == ' ' or line[i] == '\n':
-                    pass
-                elif line[i].isdigit():
-                    start = i
-                    while i + 1 < len(line) and line[i + 1].isdigit():
-                        i += 1
-                    data.append(int(line[start: i + 1]))
-                elif line[i] == ')':
-                    while opt[-1] != '(':
-                        self.process(data, opt)
-                    opt.pop()
-                elif not opt or opt[-1] == "(":
-                    opt.append(line[i])
-                elif line[i] == "(" or self.prior[line[i]] > self.prior[opt[-1]]:
-                    opt.append(line[i])
-                else:
-                    while opt and self.prior[line[i]] <= self.prior[opt[-1]]:
-                        if opt[-1] == "(":
-                            break
-                        self.process(data, opt)
-                    opt.append(line[i])
-                i += 1
-            while opt:
-                self.process(data, opt)
-        
-            print (data.pop())
-
-        r.close()
-                    
-if __name__ == '__main__':
-   A = Evaluation()
-   A.Caculator()
+        opt = []
+        data = []
+        i = 0
+        while i < len(line):
+            if line[i] == ' ' or line[i] == '\n':
+                pass
+            elif line[i].isdigit():
+                start = i
+                while i + 1 < len(line) and line[i + 1].isdigit():
+                    i += 1
+                data.append(int(line[start: i + 1]))
+            elif line[i] == ')':
+                while opt[-1] != '(':
+                    self.process(data, opt)
+                opt.pop()
+            elif not opt or opt[-1] == "(":
+                opt.append(line[i])
+            elif line[i] == "(" or self.prior[line[i]] > self.prior[opt[-1]]:
+                opt.append(line[i])
+            else:
+                while opt and self.prior[line[i]] <= self.prior[opt[-1]]:
+                    if opt[-1] == "(":
+                        break
+                    self.process(data, opt)
+                opt.append(line[i])
+            i += 1
+        while opt:
+            self.process(data, opt)
+    
+        return data.pop()
